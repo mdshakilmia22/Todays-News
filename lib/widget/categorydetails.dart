7a7612963e379e2cfa40c4eb68631c1607f98cd0
Md Shakil Mia <mdshakilmia3245@gmail.com>
2022-12-05 03:23:52 +0600

    First second search comment category appbar

--- lib/widget/categorydetails.dart
+++ lib/widget/categorydetails.dart
@@ -20,55 +20,75 @@ class CategoryDetailsPage extends StatefulWidget {
 class _CategoryDetailsPageState extends State<CategoryDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Consumer(builder: (_, ref, watch) {
-        AsyncValue<CategoryDetailsModel> first =
-            ref.watch(categoryDetailsP(widget.newsid));
-        return first.when(data: (paper) {
-          return ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: paper.datas?.data?.length ?? 0,
-              itemBuilder: (_, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Stack(alignment: Alignment.bottomLeft, children: [
-                    Container(
-                      width: double.infinity,
-                      height: 200,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                  paper.datas?.data?[index].image?[0] ?? ''))),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        paper.datas!.data![index].title.toString(),
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1.0,
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('images/timezone.png',height: 25,width: 25),
+        ),
+        title:  Padding(
+          padding: const EdgeInsets.all(0),
+          child: Text('Todays Time',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.red,fontFamily: 'Merriweather'),),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.notifications,size: 35,color: Colors.grey[600],),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Consumer(builder: (_, ref, watch) {
+          AsyncValue<CategoryDetailsModel> first =
+              ref.watch(categoryDetailsP(widget.newsid));
+          return first.when(data: (paper) {
+            return ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: paper.datas?.data?.length ?? 0,
+                itemBuilder: (_, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Stack(alignment: Alignment.bottomLeft, children: [
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    paper.datas?.data?[index].image?[0] ?? ''))),
                       ),
-                    ),
-                  ]),
-                ).onTap(()=>RiverPordDetails(endid: paper.datas!.data![index].id.toString()).launch(context));
-              });
-        }, error: (e, stack) {
-          return const Text('Error');
-        }, loading: () {
-          return Padding(
-            padding: const EdgeInsets.only(top: 300),
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        });
-      }),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          paper.datas!.data![index].title.toString(),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ]),
+                  ).onTap(()=>RiverPordDetails(endid: paper.datas!.data![index].id.toString()).launch(context));
+                });
+          }, error: (e, stack) {
+            return const Text('Error');
+          }, loading: () {
+            return Padding(
+              padding: const EdgeInsets.only(top: 300),
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          });
+        }),
+      ),
     );
   }
 }
